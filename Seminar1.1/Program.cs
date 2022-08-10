88ac268aad88562451c5c4c62f5406779cc30896
Vildan <snaiper.228@bk.ru>
2022-08-10 21:36:25 +0300

    Исправил блок схему

--- Seminar1.1/Program.cs
+++ Seminar1.1/Program.cs
@@ -2,20 +2,20 @@
 
 
 Console.WriteLine("Введите 3 числа:");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            int num3 = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-            int max = num1;
+int max = num1;
 
-            if (num2 > max)
-            {
-                max = num2;
-            }
+if (num2 > max)
+{
+    max = num2;
+}
 
-            if (num3 > max)
-            {
-                max = num3;
-            }
+if (num3 > max)
+{
+    max = num3;
+}
 
-            Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
