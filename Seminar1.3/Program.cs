6601ed619c8ebead605556af7665abd18b00bcf7
Vildan <snaiper.228@bk.ru>
2022-08-10 22:57:49 +0300

    Задание 4

---
+++ Seminar1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿int i = 1;
+int n;
+
+Console.WriteLine("Введите число: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + n);
+while (i <= n)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
