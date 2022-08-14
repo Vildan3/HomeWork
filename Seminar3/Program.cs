bf705d1babfb002ad203557351f60399eae5f446
Vildan <snaiper.228@bk.ru>
2022-08-14 16:29:19 +0300

    The first task

---
+++ Seminar3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число, чтобы проверить, является ли палиндром: ");
+bool palindrome = true;
+int x = int.Parse(Console.ReadLine());
+int c = x.ToString().Length - 1;
+string b = x.ToString();
+for (int i = 0; i < c; i++)
+    if (b[i] != b[c - i])
+        palindrome = false;
+if (palindrome == true)
+    Console.Write("Да");
+else Console.Write("Нет");
+Console.ReadKey();
