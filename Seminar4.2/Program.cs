b588b9db735b406a1668f9aa25f45f22eae14816
Vildan <snaiper.228@bk.ru>
2022-08-21 19:41:41 +0300

    The third task

---
+++ Seminar4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите метод, который задаёт массив из 8 элементов (диапазон от 1 до 100) и выводит их на экран.
+
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(1, 100);
+    Console.Write("" + Method (i) + ",");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
